e197176159ba7f65b738e63890ea892c4fb7cb14
OhSSangHoon <gnsdkaos99@naver.com>
2025-04-06 21:30:47 +0900

    style: 반응형 디자인 적용

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,10 +4,21 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-white text-black p-4 flex justify-between items-center mx-auto w-[75rem]">
-      <Link href="/">
-        <img src="/Size=Large.svg" alt="로고" className="h-10" />
-      </Link>
+    <header className="bg-white flex justify-between items-center h-16 border-b border-[#E2E8F0]">
+      <div className="mx-auto w-[75rem]">
+        <Link href="/">
+          <img
+            src="/Size=Large.svg"
+            alt="로고"
+            className="hidden sm:block h-10"
+          />
+          <img
+            src="/logo.png"
+            alt="로고" 
+            className="block sm:hidden h-10"
+          />
+        </Link>
+      </div>
     </header>
   );
 }
